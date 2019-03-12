5279d14fd0545c0decc244aea6d35d1c74ee3002
Joe Wright <joe@joejag.com>
2019-03-12 21:43:37 +0000

    Fix output. Add extra logo at top

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
 * Show stats on how many test runs - how many commits and failures
 * Make render logs as the command is running (currently it dumps sysout and syserr after the cmd finishes)
-* Check no outstanding changes in repo before starting TCR
--- src/index.js
+++ src/index.js
@@ -11,38 +11,37 @@ const Logo = () => (
 
 const LastChange = ({ path }) => (
   <Box>
-    <Color dim> last change:  </Color> {path}
+    <Color dim>last change:</Color> {path}
   </Box>
 )
 
 const RunningSummary = ({ path }) => (
-  <Box flexDirection='column' marginTop={1}>
+  <Box flexDirection='column'>
     <Box>
       <Logo />
-      <Box width={10}><Color bgYellow black width={10}> RUNNING </Color></Box>
-      <LastChange path={path} />
+      <Box width={10}><Color bgYellow black width={10}> RUNNING </Color></Box><LastChange path={path} />
     </Box>
   </Box>
 )
 
 const PassSummary = ({ path, outputText, failureText }) => (
-  <Box flexDirection='column' marginTop={1}>
+  <Box flexDirection='column'>
+    <Logo marginTop={1} />
     <Box marginTop={1}>{outputText + failureText}</Box>
     <Box marginTop={1}>
       <Logo />
-      <Box width={10}><Color bgGreen black> PASSED </Color></Box>
-      <LastChange path={path} />
+      <Box width={10}><Color bgGreen black> PASSED </Color></Box><LastChange path={path} />
     </Box>
   </Box>
 )
 
 const FailSummary = ({ path, outputText, failureText }) => (
-  <Box flexDirection='column' marginTop={1}>
+  <Box flexDirection='column'>
+    <Logo marginTop={1} />
     <FailureReason outputText={outputText} failureText={failureText} />
     <Box marginTop={1}>
       <Logo />
-      <Box width={10}><Color bgRed black width={10}> FAILED </Color></Box>
-      <LastChange path={path} />
+      <Box width={10}><Color bgRed black width={10}> FAILED </Color></Box><LastChange path={path} />
     </Box>
   </Box>
 )
@@ -63,6 +62,10 @@ if (process.argv.length !== 3) {
   process.exit(1)
 }
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 const buildAndTestCommand = process.argv.slice(2)[0]
 let ignoreNextRun = false
 
@@ -79,21 +82,32 @@ const runTCRLoop = debounce((path) => {
     git('.').add('./*').commit('working')
     render(<PassSummary path={path} outputText={runCommand.stdout} failureText={runCommand.stderr} />)
   } else {
-    ignoreNextRun = true
-    git('.').reset(['HEAD', '--hard'])
     render(<FailSummary path={path} outputText={runCommand.stdout} failureText={runCommand.stderr} />)
+    sleep(200).then(() => {
+      // sleep required as reset too quickly and editors ignore it
+      ignoreNextRun = true
+      git('.').reset(['HEAD', '--hard'])
+    })
   }
 }, 50) // wait 50ms in case multiple files have been saved
 
-git('.').status((err, statusSummary) => {
+git('.').silent(true).status((err, statusSummary) => {
   if (err) {
-    render(err)
+    render(
+      <Box flexDirection='column'>
+        <Box>
+          <Logo />
+          <Box width={10}><Color bgRed black width={10}> PROBLEM </Color></Box>
+        </Box>
+        <Box marginTop={1}>Is this a valid Git repo? TCR needs one to start. Try `git init`</Box>
+        <Box marginTop={1}>{err}</Box>
+      </Box>)
     process.exit(1)
   }
   if (statusSummary.files.length !== 0) {
-    // const problemFiles = statusSummary.files.map((file) =>
-    //   <Color key={file} red><span> * {file}</span></Color>
-    // )
+    const problemFiles = statusSummary.files.map((file) =>
+      <Box key={file.path}> * <Color red>{file.path}</Color></Box>
+    )
     render(
       <Box flexDirection='column'>
         <Box>
@@ -101,7 +115,7 @@ git('.').status((err, statusSummary) => {
           <Box width={10}><Color bgRed black width={10}> PROBLEM </Color></Box>
         </Box>
         <Box marginTop={1}>There are files already changed in this repo, please commit them before starting a TCR session</Box>
-        {/* <Box>{problemFiles}</Box> */}
+        {<Box flexDirection='column' marginTop={1} marginBottom={1}>{problemFiles}</Box>}
       </Box>
     )
     process.exit(1)
