2f37b020c687cc3cc44016b4cc3fe9e44803f6d3
Joe Wright <joseph.wright@ros.gov.uk>
2019-03-12 13:57:36 +0000

    Add better example. Extract some React components

--- src/index.js
+++ src/index.js
@@ -5,63 +5,71 @@ import debounce from 'debounce'
 import shell from 'shelljs'
 import git from 'simple-git'
 
+const Logo = () => (
+  <Box> TCR </Box>
+)
+
+const LastChange = ({ path }) => (
+  <Box>
+    <Color dim> last change:  </Color> {path}
+  </Box>
+)
+
 const RunningSummary = ({ path }) => (
   <Box flexDirection='column' marginTop={1}>
     <Box>
-      <Box bold> TCR  </Box>
+      <Logo />
       <Box width={10}><Color bgYellow black width={10}> RUNNING </Color></Box>
-      <Color dim> last change:  </Color> {path}
+      <LastChange path={path} />
     </Box>
   </Box>
 )
 
 const PassSummary = ({ path, outputText, failureText }) => (
   <Box flexDirection='column' marginTop={1}>
-    <Box>
-      <Box bold> TCR  </Box>
-      <Box width={10}><Color bgGreen black> PASSED </Color></Box>
-      <Color dim> last change:  </Color> {path}
-    </Box>
-
     <Box marginTop={1}>{outputText + failureText}</Box>
+    <Box marginTop={1}>
+      <Logo />
+      <Box width={10}><Color bgGreen black> PASSED </Color></Box>
+      <LastChange path={path} />
+    </Box>
   </Box>
 )
 
 const FailSummary = ({ path, outputText, failureText }) => (
   <Box flexDirection='column' marginTop={1}>
-    <Box>
-      <Box bold> TCR  </Box>
-      <Box width={10}><Color bgRed black width={10}> FAILED </Color></Box>
-      <Color dim> last change:  </Color> {path}
-    </Box>
-
     <FailureReason outputText={outputText} failureText={failureText} />
+    <Box marginTop={1}>
+      <Logo />
+      <Box width={10}><Color bgRed black width={10}> FAILED </Color></Box>
+      <LastChange path={path} />
+    </Box>
   </Box>
 )
 
 const FailureReason = ({ outputText, failureText }) => (
-  <Box marginTop={1}>{outputText + failureText}</Box>
+  <Box>{outputText + failureText}</Box>
 )
 
 // TODO: maybe: process all the args to save the user putting thier test command in quotes?
 const buildAndTestCommand = process.argv.slice(2)[0]
-let lastFailure = <div />
+let ignoreNextRun = false
 
 const runTCRLoop = debounce((path) => {
-  render(<RunningSummary path={path} />)
+  if (ignoreNextRun === true) {
+    ignoreNextRun = false
+    return
+  }
 
+  render(<RunningSummary path={path} />)
   const runCommand = shell.exec(buildAndTestCommand, { silent: true })
 
   if (runCommand.code === 0) {
     git('.').add('./*').commit('working')
-    render(<div>
-      <PassSummary path={path} outputText={runCommand.stdout} failureText={runCommand.stderr} />
-      <Color dim>Last failure output:</Color>
-      {lastFailure}
-    </div>)
+    render(<PassSummary path={path} outputText={runCommand.stdout} failureText={runCommand.stderr} />)
   } else {
+    ignoreNextRun = true
     git('.').reset(['HEAD', '--hard'])
-    lastFailure = <FailureReason outputText={runCommand.stdout} failureText={runCommand.stderr} />
     render(<FailSummary path={path} outputText={runCommand.stdout} failureText={runCommand.stderr} />)
   }
 }, 50) // wait 50ms in case multiple files have been saved
--- test/example_file.rb
+++ test/example_file.rb
@@ -1,4 +1,58 @@
-sleep 1
-$stderr.puts 'bad'
-puts 'good'
-exit 0
+require 'minitest/autorun'
+
+class Bowling
+
+  def score(frames)
+    frames.each_with_index.inject(0) do |sum, (this_frame, idx)| 
+        next_two_balls = frames.fetch(idx+1, [0]).concat([0,0]).concat([0,0]).first(2)
+        bonus = calculate_bonus(this_frame, next_two_balls, idx)
+        sum + sum_frame(this_frame) + bonus
+    end
+  end
+
+  def calculate_bonus(this_frame, next_frame, idx)
+    if this_frame[0] == 10
+        return sum_frame(next_frame.first(2)) 
+    end 
+    if sum_frame(this_frame) == 10
+        return next_frame[0]
+    end
+    0
+  end
+
+  def sum_frame(frame)
+    frame.inject(0){|sum, x| sum + x }
+  end
+end
+
+class BlehTest < Minitest::Test 
+  def test_gutter_ball_run
+    assert 0 == Bowling.new.score(
+        [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
+    )
+  end
+
+  def test_threes
+    assert 9 == Bowling.new.score([[3, 3], [3, 0]])
+  end
+
+  def test_spares
+    assert 20 == Bowling.new.score([[0, 10], [5, 0]])
+    assert 22 == Bowling.new.score([[1, 9], [6, 0]])
+  end
+
+  def test_last_frame_when_previous_was_a_spare
+    assert 140 == Bowling.new.score(
+        [[4,6], [4,6], [4,6], [4,6], [4,6], [4,6], [4,6], [4,6], [4,6], [4,6,4]]
+    )
+  end
+
+  def test_strike
+    assert 22 == Bowling.new.score([[10], [5, 1]])
+    #assert 46 == Bowling.new.score([[10], [10], [3,3]])
+  end
+
+  def test_perfect_game
+    # assert 240 == Bowling.new.score([[10], [10], [10], [10], [10], [10], [10], [10], [10], [0,0])
+  end
+end
