90dca4ff25aad0164154d85503832d55ab7b4ea6
Joe Wright <joe@joejag.com>
2019-03-17 18:12:24 +0000

    Check langauge is available

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import { createSample } from './tech/samples'
 import { runTcr } from './tcr'
 import { NotEnoughArgumentsError } from './components'
 
+const availableLanguages = ['csharp', 'java', 'node', 'python', 'ruby']
+
 if (process.argv.length === 3 && process.argv.slice(2)[0].startsWith('--')) {
   const language = process.argv.slice(2)[0].replace('--', '')
-  createSample(language, () => {
-    runTcr('./runTests.sh')
-  })
+  if (availableLanguages.indexOf(language) > -1) {
+    createSample(language, () => {
+      runTcr('./runTests.sh')
+    })
+  } else {
+    console.log('Sorry, only these languages are supported for samples', availableLanguages)
+    process.exit(1)
+  }
 } else if (process.argv.length === 3) {
   runTcr(process.argv.slice(2)[0])
 } else {
